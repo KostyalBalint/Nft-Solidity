284fb2880cf68a4509b75d8e226caad5a49cc276
Kostyal Balint <kostyal.balintnak@gmail.com>
2021-05-10 12:35:23 +0200

    Add basic test to the contract

--- test/ImageNft.test.js
+++ test/ImageNft.test.js
@@ -1,32 +1,45 @@
 const ImageNft = artifacts.require('ImageNft');
+const { assert } = require('chai');
 const fetch = require('node-fetch');
 
 contract('ImageNft', (accounts) => {
-  it('shoud deply sucessfully', async () => {
+  let imageNftContract;
 
-
+  //Deplye the contract once before the tests
+  before(async () => {
+    imageNftContract = await ImageNft.deployed();
   });
 
-  it('shoud safeMint the 0-th token to the first address', async () => {
-    const imageNftInstance = await ImageNft.deployed();
-    // const accounts = await web3.eth.getAccounts();
+  it('shoud deply sucessfully', async () => {
+    const address = imageNftContract.address;
+    assert.notEqual(address, 0x0);
+    assert.notEqual(address, '');
+    assert.notEqual(address, null);
+    assert.notEqual(address, undefined);
+  });
+
+  it('shoud have a name and symbol', async () => {
+    const name = await imageNftContract.name();
+    const symbol = await imageNftContract.symbol();
+    assert.equal(name, 'ImageNft');
+    assert.equal(symbol, 'IMG');
+  });
+
+  it('shoud mint the 0-th token to the first address', async () => {
     const uriStr = 'token-uri-1';
     const baseUri = 'https://my-json-server.typicode.com/KostyalBalint/Nft-Solidity/';
 
-    const token = await imageNftInstance.mint(uriStr, {'from': accounts[2]});
-    console.log(token);
+    const token = await imageNftContract.mint(uriStr);
 
-    assert.equal(await imageNftInstance.tokenURI(0), baseUri + uriStr, `Token 0 shoud have URI String: ${baseUri + uriStr}`);
+    assert.equal(await imageNftContract.tokenURI(0), baseUri + uriStr, `Token 0 shoud have URI String: ${baseUri + uriStr}`);
   });
 
-  it(' token 0 uri should return image url in the json response', async () => {
-    const imageNftInstance = await ImageNft.deployed();
-    ;
+  it('should return image url in the json response for token 0', async () => {
     // Create token
-    const token = await imageNftInstance.mint('token-uri-1');
+    const token = await imageNftContract.mint('token-uri-1');
 
     // Get URI back
-    const tokenUri = await imageNftInstance.tokenURI(0);
+    const tokenUri = await imageNftContract.tokenURI(0);
 
     // Fetch the Json data from the uri
     const tokenJson = await fetch(tokenUri).then((res) => res.json());
