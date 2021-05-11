746f5c3c9f6ce6541c103a3fd7efb5937be37af8
Kostyal Balint <kostyal.balintnak@gmail.com>
2021-05-11 12:52:27 +0200

    Test refactoring, add test for transfer

--- contracts/ImageNft.sol
+++ contracts/ImageNft.sol
@@ -3,13 +3,15 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract ImageNft is ERC721, ERC721URIStorage, Ownable {
+      using Counters for Counters.Counter;
+      Counters.Counter private _tokenIds;
+      mapping(string => bool) _uriExists;
 
-      uint256 nextId;   //TODO: Use openzeppelin counter class
-      constructor() ERC721("ImageNft", "IMG") {
-            nextId = 0;
-      }
+      //uint256 nextId;   //TODO: Use openzeppelin counter class
+      constructor() ERC721("ImageNft", "IMG") {}
 
       function _baseURI() internal view virtual override returns (string memory) {
             //Fake Json URI resource data can be found under db.json file
@@ -19,11 +21,16 @@ contract ImageNft is ERC721, ERC721URIStorage, Ownable {
       function mint(string memory _tokenURI) public payable returns (uint256) {
             //The creation of this token cost's a fixed 0.01 Ether
             require(msg.value == 0.01 ether);
+            require(!_uriExists[_tokenURI]);    //Would be better to check the hash of the img on the uri
 
-            _safeMint(msg.sender, nextId);
-            _setTokenURI(nextId, _tokenURI);
-            nextId++;
-            return nextId-1;
+            _tokenIds.increment();
+            uint256 newItemId = _tokenIds.current();
+
+            _safeMint(msg.sender, newItemId);
+            _setTokenURI(newItemId, _tokenURI);
+            _uriExists[_tokenURI] = true;
+            
+            return newItemId;
       }
 
       function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
--- test/ImageNft.test.js
+++ test/ImageNft.test.js
@@ -6,59 +6,77 @@ contract('ImageNft', (accounts) => {
   let imageNftContract;
 
   //Deplye the contract once before the tests
-  before(async () => {
-    //TODO: remove state after each test 
-    imageNftContract = await ImageNft.new();
-    //imageNftContract = await ImageNft.deployed();
+
+  beforeEach(async () => {
+    ImageNft.new();
+    imageNftContract = await ImageNft.deployed();
   });
 
-  it('shoud deploy sucessfully', async () => {
-    const address = imageNftContract.address;
-    assert.notEqual(address, 0x0);
-    assert.notEqual(address, '');
-    assert.notEqual(address, null);
-    assert.notEqual(address, undefined);
+  describe('depoloyment', async () => {
+    it('shoud deploy sucessfully', async () => {
+      const address = imageNftContract.address;
+      assert.notEqual(address, 0x0);
+      assert.notEqual(address, '');
+      assert.notEqual(address, null);
+      assert.notEqual(address, undefined);
+    });
+  
+    it('shoud have a name and symbol', async () => {
+      const name = await imageNftContract.name();
+      const symbol = await imageNftContract.symbol();
+      assert.equal(name, 'ImageNft');
+      assert.equal(symbol, 'IMG');
+    });
   });
 
-  it('shoud have a name and symbol', async () => {
-    const name = await imageNftContract.name();
-    const symbol = await imageNftContract.symbol();
-    assert.equal(name, 'ImageNft');
-    assert.equal(symbol, 'IMG');
+  describe('minting', () => {
+    it('should cost 0.01 ether to mint a token', async () => {
+      let value = web3.utils.toWei('0.01', 'ether');
+      let balanceBefore = await web3.eth.getBalance(accounts[2]);
+      //Gas price set to 0 here to properly test the price
+      let tokenResult = await imageNftContract.mint('token-uri-2', {from: accounts[2], value, gasPrice: 0});
+      let balanceAfter = await web3.eth.getBalance(accounts[2]);
+  
+      assert.equal(balanceBefore - balanceAfter, value, 'Minting a token should const 0.01 ether');
+      //TODO: test transaction with lower price
+    });
+  
+    it('should have proper uri string of a minted token', async () => {
+      const uriStr = 'token-uri-1';
+      const baseUri = 'https://my-json-server.typicode.com/KostyalBalint/Nft-Solidity/';
+      
+      const result = await imageNftContract.mint(uriStr, {from: accounts[0], value: web3.utils.toWei('0.01', 'ether')});
+      const tokenId = result.logs[0].args.tokenId;
+  
+      assert.equal(await imageNftContract.tokenURI(tokenId), baseUri + uriStr, `Token 0 shoud have URI String: ${baseUri + uriStr}`);
+    });
+  
+    it('should return image url in the json response', async () => {
+      // Create token
+      const result = await imageNftContract.mint('token-uri-3', {from: accounts[0], value: web3.utils.toWei('0.01', 'ether')});
+
+      // Get the token id
+      const tokenId = result.logs[0].args.tokenId; 
+  
+      // Get URI back
+      const tokenUri = await imageNftContract.tokenURI(tokenId);
+  
+      // Fetch the Json data from the uri
+      const tokenJson = await fetch(tokenUri).then((res) => res.json());
+  
+      assert.equal(tokenJson.image, 'image url here');
+    });
+  })
+
+  describe('transfer', async () => {
+    it('should be transferable between users', async () => {
+      let ownerBefore = await imageNftContract.ownerOf(1);
+      await imageNftContract.transferFrom(ownerBefore, accounts[4], 1, {from: ownerBefore});
+      let ownerAfter = await imageNftContract.ownerOf(1);
+      assert.equal(ownerAfter, accounts[4], "Should transfer token between users");
+    });
   });
 
-  it('should cost 0.01 ether to mint a token', async () => {
-    let value = web3.utils.toWei('0.01', 'ether');
-    let balanceBefore = await web3.eth.getBalance(accounts[2]);
-    //Gas price set to 0 here to properly test the price
-    let tokenResult = await imageNftContract.mint('token-uri-1', {from: accounts[2], value, gasPrice: 0});
-    let balanceAfter = await web3.eth.getBalance(accounts[2]);
-
-    assert.equal(balanceBefore - balanceAfter, value, 'Minting a token should const 0.01 ether');
-    //TODO: test transaction with lower price
-  });
-
-  it('shoud have proper uri string of a minted token', async () => {
-    const uriStr = 'token-uri-1';
-    const baseUri = 'https://my-json-server.typicode.com/KostyalBalint/Nft-Solidity/';
-
-    await imageNftContract.mint.call(uriStr, {from: accounts[0], value: web3.utils.toWei('0.01', 'ether')});
-
-    assert.equal(await imageNftContract.tokenURI(0), baseUri + uriStr, `Token 0 shoud have URI String: ${baseUri + uriStr}`);
-  });
-
-  it('should return image url in the json response for token 0', async () => {
-    // Create token
-    await imageNftContract.mint('token-uri-1', {from: accounts[0], value: web3.utils.toWei('0.01', 'ether')});
-
-    // Get URI back
-    const tokenUri = await imageNftContract.tokenURI(0);
-
-    // Fetch the Json data from the uri
-    const tokenJson = await fetch(tokenUri).then((res) => res.json());
-
-    assert.equal(tokenJson.image, 'https://pic.weblogographic.com/img/technology-2017/difference-between-uri-and-url.jpg');
-  });
 });
 
 // TODO: Test: should have base uri
